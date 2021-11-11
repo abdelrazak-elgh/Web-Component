a38cec322c037c1cd911bcf4dabd5a4ddb48c72b
abdelrazak-elgh <abdelrazak.elghazzaz@gmail.com>
2021-11-11 03:54:58 +0100

    ajout de l'affichage de la durée et temps passé de la vidéo

--- MyComponent/index.js
+++ MyComponent/index.js
@@ -14,14 +14,21 @@ let style = `
   }
 `;
 let template = /*html*/`
-    <video id="player" >
+    <video id="video" >
     </video>
     <br>
     <button id="play">PLAY</button>
     <button id="pause" hidden>PAUSE</button>
     <button id="info">GET INFO</button>
-    <button id="fwd-10s">+10S</button>
-    <button id="spd-x4" >VIT X4</button>
+    <button id="mb-5s">-5S</button>
+    <button id="fwd-5s">+5S</button>
+    <button id="speed" >SPEED</button>
+    <input type="range" id="volume-bar" title="volume" min="0" max="1" step="0.1" value="1">
+    <div class="time">
+      <time id="time-elapsed">00:00</time>
+      <span> / </span>
+      <time id="duration">00:00</time>
+    </div>
    `;
 export default class MyVideoPlayer extends HTMLElement {
   constructor() {
@@ -29,19 +36,30 @@ export default class MyVideoPlayer extends HTMLElement {
     console.log("BaseURL = " + getBaseURL());
     const shadow = this.attachShadow({ mode: 'open' });
   }
+
   connectedCallback() {
     this.shadowRoot.innerHTML = `<style>${style}</style>${template}`;
 
-    this.player = this.shadowRoot.querySelector("#player");
-    this.playButton = this.shadowRoot.querySelector("#play");
-    //this.playButton.hidden = false;
-    this.pauseButton = this.shadowRoot.querySelector("#pause");
+    // recuperation des elements du shadow
+    this.player = this.getSDom('#video');
+    this.timeElapsed = this.getSDom('#time-elapsed');
+    this.duration = this.getSDom('#duration');
+
+    this.playButton = this.getSDom('#play');
+    this.pauseButton = this.getSDom('#pause');
+    this.fwd5sButton = this.getSDom('#fwd-5s');
+    this.mb5sButton = this.getSDom('#mb-5s');
+    this.spdButton = this.getSDom('#speed');
 
     // récupération de l'attribut HTML
     this.player.src = this.getAttribute("src");
 
+    // configuration des ecouteurs sur les boutons
     this.listenerConfiguration();
+  }
 
+  getSDom(selector) {
+    return this.shadowRoot.querySelector(selector);
   }
 
   listenerConfiguration() {
@@ -56,6 +74,26 @@ export default class MyVideoPlayer extends HTMLElement {
       this.pauseButton.hidden = true;
       this.playButton.hidden = false;
     }
+
+    this.fwd5sButton.onclick = () => {
+      this.forward5s();
+    }
+
+    this.mb5sButton.onclick = () => {
+      this.moveBack5s();
+    }
+
+    this.spdButton.onclick = () => {
+      this.speed();
+    }
+
+    this.player.onloadedmetadata = () => {
+      this.initializeVideo();
+    }
+
+    this.player.ontimeupdate = () => {
+      this.updateTimeElapsed();
+    }
   }
 
   play() {
@@ -64,6 +102,41 @@ export default class MyVideoPlayer extends HTMLElement {
 
   pause() {
     this.player.pause();
+  }
+
+  forward5s() {
+    this.player.currentTime += 5;
+  }
+
+  moveBack5s() {
+    this.player.currentTime -= 5;
+  }
+
+  speed() {
+    this.player.playbackRate += 0.5;
+  }
+
+  formatTime(timeInSeconds) {
+    let result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
+
+    return {
+      minutes: result.substr(3, 2),
+      seconds: result.substr(6, 2),
+    };
+  }
+
+  initializeVideo() {
+    console.log(this.player.duration);
+    let videoDuration = Math.round(this.player.duration);
+    let time = this.formatTime(videoDuration);
+    this.duration.innerText = `${time.minutes}:${time.seconds}`;
+    this.duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`);
+  }
+
+  updateTimeElapsed() {
+    const time = this.formatTime(Math.round(this.player.currentTime));
+    this.timeElapsed.innerText = `${time.minutes}:${time.seconds}`;
+    this.timeElapsed.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
   }
 }
 
