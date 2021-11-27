954106871ed4abdda628a3111ebef9b762ae3ccf
abdelrazak-elgh <abdelrazak.elghazzaz@gmail.com>
2021-11-27 04:42:32 +0100

    video local + knobs + visu frequence + maj affichage

---
+++ MyComponent/js/htmlTemplate.js
@@ -0,0 +1,363 @@
+const template = /*html*/`
+<script src=".lib/webaudio-controls.js"></script>
+<div class="container">
+    <div class="main-video-container">
+      <div class="sub-mvc-1">
+        <div class="video-container">
+          <video class="currentVideo"
+                src="MyComponent/assets/video/FREE_Guitar_Beat.mp4" type="video/mp4"
+          ></video>
+          <div class="video-controls">
+            <div class="control-items">
+              <div class="control-bar-left elements">
+                  <button class="btooltip" id="play">
+                    <span class="tooltiptext">play</span
+                    ><i class="fa fa-play"></i>
+                  </button>
+                  <button class="btooltip" id="pause" hidden>
+                    <span class="tooltiptext">pause</span
+                    ><i class="fa fa-pause"></i>
+                  </button>
+                  <button class="btooltip" id="mb-5s">
+                    <span class="tooltiptext">move back 5s</span
+                    ><i class="fa fa-fast-backward"></i>
+                  </button>
+                  <button class="btooltip" id="fwd-5s">
+                    <span class="tooltiptext">move forward 5s</span
+                    ><i class="fa fa-fast-forward"></i>
+                  </button>
+
+                <div class="time elements">
+                  <time id="time-elapsed">00:00</time>
+                  <span>/</span>
+                  <time id="duration">00:00</time>
+                </div>
+              </div>
+
+              <div class="control-bar-right">
+                <div class="volume-setting elements">
+                  <button class="btooltip" id="v-on">
+                    <span class="tooltiptext">mute (m)</span
+                    ><i class="fa fa-volume-up"></i>
+                  </button>
+                  <button class="btooltip" id="v-off" hidden>
+                    <span class="tooltiptext">unmute (m)</span>
+                    <i class="fa fa-volume-off"></i>
+                  </button>
+                  <div class="volumebar">
+                    <input
+                      type="range"
+                      class="custom-slider"
+                      title="volume"
+                      min="0"
+                      max="1"
+                      step="0.01"
+                      value="0.5"
+                    />
+                  </div>
+                </div>
+                <div class="elements">
+                  <button
+                    class="btooltip"
+                    id="fullscreen"
+                    aria-label="fullscreen"
+                  >
+                    <span class="tooltiptext">fullscreen (f)</span>
+                    <i class="fa fa-expand"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="video-progress">
+              <div class="video-progress-filled"></div>
+            </div>
+          </div>
+        </div>
+        <h3 class="video-data title">Raindrops</h3>
+      </div>
+      <div class="sub-mvc-2">
+        <button id="hide-knobs" hidden>
+          <i class="fa fa-caret-down"></i>
+          <span>Knobs Equalizer</span>
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="knobs-div">
+          <div class="knobs-controls">
+            <label>60Hz</label>
+            <webaudio-knob id="knb1"
+              src="MyComponent/assets/knobs/LittlePhatty.png" diameter="64"
+              sprites="100" value="0"
+              min="-30" max="30" step="1">
+            </webaudio-knob>
+            <output id="gain0">0 dB</output>
+          </div>
+          <div class="knobs-controls">
+            <label>170Hz</label>
+            <webaudio-knob id="knb2"
+              src="MyComponent/assets/knobs/LittlePhatty.png" diameter="64"
+              sprites="100" value="0"
+              min="-30" max="30" step="1">
+            </webaudio-knob>
+            <output id="gain1">0 dB</output>
+          </div>
+          <div class="knobs-controls">
+            <label>350Hz</label>
+            <webaudio-knob id="knb3"
+              src="MyComponent/assets/knobs/LittlePhatty.png" diameter="64"
+              sprites="100" value="0"
+              min="-30" max="30" step="1">
+            </webaudio-knob>
+            <output id="gain2">0 dB</output>
+          </div>
+          <div class="knobs-controls">
+            <label>1000Hz</label>
+            <webaudio-knob id="knb4"
+              src="MyComponent/assets/knobs/LittlePhatty.png" diameter="64"
+              sprites="100" value="0"
+              min="-30" max="30" step="1">
+            </webaudio-knob>
+            <output id="gain3">0 dB</output>
+          </div>
+          <div class="knobs-controls">
+            <label>3500Hz</label>
+            <webaudio-knob id="knb5"
+              src="MyComponent/assets/knobs/LittlePhatty.png" diameter="64"
+              sprites="100" value="0"
+              min="-30" max="30" step="1">
+            </webaudio-knob>
+            <output id="gain4">0 dB</output>
+          </div>
+          <div class="knobs-controls">
+            <label>10000Hz</label>
+            <webaudio-knob id="knb6"
+              src="MyComponent/assets/knobs/LittlePhatty.png" diameter="64"
+              sprites="100" value="0"
+              min="-30" max="30" step="1">
+            </webaudio-knob>
+            <output id="gain5">0 dB</output>
+          </div>
+        </div>
+        <button id="display-knobs">
+        <i class="fa fa-caret-up"></i>
+        <span>Knobs Equalizer</span>
+        <i class="fa fa-caret-up"></i>
+        </button>
+      </div>
+    </div>
+
+    <div class="video-list-container">
+      <div class="sub-vlc-1">
+        <div class="header-video-list">
+          Playlist
+        </div>
+        <div class="video-list">
+          <div class="vid active">
+            <video 
+              src="MyComponent/assets/video/FREE_Guitar_Beat.mp4">
+            </video>
+            <div class="video-info-div">
+              <div>
+                <h3 class="video-data title">sintel</h3>
+              </div>
+              <div>
+                <p class="video-data author">By Author</p>
+              </div>
+              <div class="duree">
+                <p class="video-data">00:00</p>
+              </div>
+            </div>
+          </div>
+          <div class="vid">
+            <video 
+                src="MyComponent/assets/video/Overtaken.mp4" type="video/mp4">
+            </video>
+            <div class="video-info-div">
+               <div>
+                <h3 class="video-data title">Demon slayer</h3>
+              </div>
+              <div>
+                <p class="video-data author">By Author</p>
+              </div>
+              <div class="duree">
+                <p class="video-data">00:00</p>
+              </div>
+            </div>
+          </div>
+          <div class="vid">
+            <video 
+                src="MyComponent/assets/video/Demon_slayer.mp4" type="video/mp4">
+            </video>
+            <div class="video-info-div">
+               <div>
+                <h3 class="video-data title">Demon slayer</h3>
+              </div>
+              <div>
+                <p class="video-data author">By Author</p>
+              </div>
+              <div class="duree">
+                <p class="video-data">00:00</p>
+              </div>
+            </div>
+          </div>
+          <div class="vid">
+            <video 
+                src="MyComponent/assets/video/FREE_Guitar_Beat_2.mp4" type="video/mp4">
+            </video>
+            <div class="video-info-div">
+               <div>
+                <h3 class="video-data title">Demon slayer</h3>
+              </div>
+              <div>
+                <p class="video-data author">By Author</p>
+              </div>
+              <div class="duree">
+                <p class="video-data">00:00</p>
+              </div>
+            </div>
+          </div>
+          <div class="vid">
+            <video 
+              src="MyComponent/assets/video/Sintel_trailer.mp4" type="video/mp4"
+              muted>
+            </video>
+            <div class="video-info-div">
+              <div>
+                <h3 class="video-data title">sintel</h3>
+              </div>
+              <div>
+                <p class="video-data author">By Author</p>
+              </div>
+              <div class="duree">
+                <p class="video-data">00:00</p>
+              </div>
+            </div>
+          </div>
+          <div class="vid">
+            <video 
+              src="MyComponent/assets/video/Raindrops.mp4" type="video/mp4"
+              muted>
+            </video>
+            <div class="video-info-div">
+              <div>
+                <h3 class="video-data title">Raindrops</h3>
+              </div>
+              <div>
+                <p class="video-data author">By Author</p>
+              </div>
+              <div class="duree">
+                <p class="video-data">00:00</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="vid">
+            <video 
+              src="MyComponent/assets/video/fireworks.mp4" type="video/mp4"
+              muted>
+            </video>
+            <div class="video-info-div">
+              <div>
+                <h3 class="video-data title">Fireworks</h3>
+              </div>
+              <div>
+                <p class="video-data author">By Author</p>
+              </div>
+              <div class="duree">
+                <p class="video-data">00:00</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="vid">
+            <video 
+              src="MyComponent/assets/video/big_buck_bunny.mp4" type="video/mp4"
+              muted>
+            </video>
+            <div class="video-info-div">
+              <div>
+                <h3 class="video-data title">Big buck bunny</h3>
+              </div>
+              <div>
+                <p class="video-data author">By Author</p>
+              </div>
+              <div class="duree">
+                <p class="video-data">00:00</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="vid">
+            <video 
+              src="MyComponent/assets/video/Climate_Change_Protests.mp4" type="video/mp4" 
+              muted>
+            </video>
+            <div class="video-info-div">
+              <div>
+                <h3 class="video-data title">Climate Change Protests</h3>
+              </div>
+              <div>
+                <p class="video-data author">By Author</p>
+              </div>
+              <div class="duree">
+                <p class="video-data">00:00</p>
+              </div>
+            </div>
+          </div>
+        
+          <div class="vid">
+            <video 
+              src="MyComponent/assets/video/trafficjam_sounds.mp4" type="video/mp4"
+              muted>
+            </video>
+            <div class="video-info-div">
+              <div>
+                <h3 class="video-data title">cars sound</h3>
+              </div>
+              <div>
+                <p class="video-data author">By Author</p>
+              </div>
+              <div class="duree">
+                <p class="video-data">00:00</p>
+              </div>
+            </div>
+          </div>
+        
+          <div class="vid">
+            <video 
+              src="MyComponent/assets/video/Grills.mp4" type="video/mp4"
+              muted>
+            </video>
+            <div class="video-info-div">
+              <div>
+                <h3 class="video-data title">Veritable grill</h3>
+              </div>
+              <div>
+                <p class="video-data author">By Author</p>
+              </div>
+              <div class="duree">
+                <p class="video-data">00:00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sub-vlc-2">
+        <button id="hide-sound-graph" hidden>
+          <i class="fa fa-caret-down"></i>
+          <span>2D audio vizualization</span>
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="canvas-div" hidden>
+          <canvas id="myCanvas" ></canvas>
+        </div>
+        <button id="display-sound-graph">
+          <i class="fa fa-caret-up"></i>
+          <span>2D audio vizualization</span>
+          <i class="fa fa-caret-up"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+`;
+
+export { template };
---
+++ MyComponent/main.js
@@ -0,0 +1,495 @@
+import { style } from './js/cssTemplate.js';
+import { template } from './js/htmlTemplate.js'
+import { visualize2d } from './js/webAudioVisualiseur.js'
+import './js/lib/webaudio-controls.js';
+
+const getBaseURL = () => {
+  return new URL('.', import.meta.url);
+};
+
+export default class MyVideoPlayer extends HTMLElement {
+  constructor() {
+    super();
+    console.log("BaseURL = " + getBaseURL());
+    const shadow = this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.shadowRoot.innerHTML = `<style>${style}</style>${template}`;
+
+    // recuperation des elements du shadow
+    this.listVideo = this.shadowRoot.querySelectorAll('.vid');
+    this.listVideoDuration = this.shadowRoot.querySelectorAll('.lv-duration');
+
+    this.videoContainer = this.getSDom('.video-container');
+    this.videoControls = this.getSDom('.video-controls');
+    this.video = this.getSDom('.currentVideo');
+    //this.video = this.shadowRoot.getElementById('currentVideo');
+    this.timeElapsed = this.getSDom('#time-elapsed');
+    this.duration = this.getSDom('#duration');
+    this.rangeInput = this.getSDom('.custom-slider');
+    this.progress = this.getSDom('.video-progress');
+    this.progressBar = this.getSDom('.video-progress-filled');
+
+    this.playButton = this.getSDom('#play');
+    this.pauseButton = this.getSDom('#pause');
+    this.fwd5sButton = this.getSDom('#fwd-5s');
+    this.mb5sButton = this.getSDom('#mb-5s');
+    //this.spdButton = this.getSDom('#speed');
+    this.volumeOn = this.getSDom('#v-on');
+    this.volumeOff = this.getSDom("#v-off");
+    this.fullScreenButton = this.getSDom('#fullscreen');
+
+    this.displayKnobsButton = this.getSDom('#display-knobs');
+    this.hideKnobsButton = this.getSDom('#hide-knobs');
+    this.subMVC1 = this.getSDom('.sub-mvc-1');
+    this.subMVC2 = this.getSDom('.sub-mvc-2');
+
+    this.displaySoundGraph = this.getSDom('#display-sound-graph');
+    this.hideSoundGraph = this.getSDom('#hide-sound-graph');
+    this.subVLC1 = this.getSDom('.sub-vlc-1');
+    this.subVLC2 = this.getSDom('.sub-vlc-2');
+
+    // récupération de l'attribut HTML
+    // this.video.src = this.getAttribute("src");
+
+    // initialisation du volume de la video par default
+    this.video.volume = this.rangeInput.value;
+
+    this.filters = [];
+
+    this.audioCtx = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext;
+    this.audioContext = new this.audioCtx();
+
+    this.knobsDiv = this.getSDom('.knobs-div');;
+    this.knob1 = this.getSDom('#knb1');
+    this.knob2 = this.getSDom('#knb2');
+    this.knob3 = this.getSDom('#knb3');
+    this.knob4 = this.getSDom('#knb4');
+    this.knob5 = this.getSDom('#knb5');
+    this.knob6 = this.getSDom('#knb6');
+
+    this.canvasDiv = this.getSDom('.canvas-div');
+    this.canvas = this.getSDom('#myCanvas');
+    this.canvasContext = this.canvas.getContext('2d');
+
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+
+    // configuration des ecouteurs sur les boutons
+    this.listenerConfiguration();
+
+    this.buildAudioGraph();
+
+    this.visualizeFrequencies = () => {
+      requestAnimationFrame(this.visualizeFrequencies);
+      this.canvasContext.fillStyle = 'rgb(0,0,0)';
+      this.canvasContext.fillRect(0, 0, this.width, this.height);
+
+      this.analyser.getByteFrequencyData(this.dataArray)
+
+      var barWidth = this.width / this.bufferLength;
+      var barHeight;
+      var x = 0;
+
+      // values go from 0 to 256 and the canvas heigt is 100. Let's rescale
+      // before drawing. This is the scale factor
+      let heightScale = this.height / 128;
+
+      for (var i = 0; i < this.bufferLength; i++) {
+        barHeight = this.dataArray[i];
+
+
+        this.canvasContext.fillStyle = 'rgb(252, 194, ' + (barHeight) + ')';
+        barHeight *= heightScale;
+        this.canvasContext.fillRect(x, this.height - barHeight / 2, barWidth, barHeight / 2);
+
+        // 2 is the number of pixels between bars
+        x += barWidth + 1;
+      }
+    }
+    requestAnimationFrame(this.visualizeFrequencies);
+  }
+
+  buildAudioGraph() {
+    this.video.onplay = (e) => { this.audioContext.resume(); }
+
+    // fix for autoplay policy
+    this.video.addEventListener('play', () => this.audioContext.resume());
+
+    let sourceNode = this.audioContext.createMediaElementSource(this.video);
+
+    // Create an analyser node
+    this.analyser = this.audioContext.createAnalyser();
+
+    // Try changing for lower values: 512, 256, 128, 64...
+    this.analyser.fftSize = 512;
+    this.bufferLength = this.analyser.frequencyBinCount;
+    this.dataArray = new Uint8Array(this.bufferLength);
+
+    // sourceNode.connect(this.analyser);
+
+    // create the equalizer. It's a set of biquad Filters
+
+
+    // Set filters
+    [60, 170, 350, 1000, 3500, 10000].forEach((freq, i) => {
+      let eq = this.audioContext.createBiquadFilter();
+      eq.frequency.value = freq;
+      eq.type = "peaking";
+      eq.gain.value = 0;
+      this.filters.push(eq);
+    });
+
+    // Connect filters in serie
+    sourceNode.connect(this.filters[0]);
+    for (let i = 0; i < this.filters.length - 1; i++) {
+      this.filters[i].connect(this.filters[i + 1]);
+    }
+
+    // connect the last filter to the speakers
+    this.filters[this.filters.length - 1].connect(this.analyser);
+
+    this.analyser.connect(this.audioContext.destination);
+  }
+
+  changeGain(sliderVal, nbFilter) {
+    let value = sliderVal;
+    this.filters[nbFilter].gain.value = value;
+
+    // update output labels
+    let output = this.getSDom("#gain" + nbFilter);
+    output.value = value + " dB";
+  }
+
+  getSDom(selector) {
+    return this.shadowRoot.querySelector(selector);
+  }
+
+  listenerConfiguration() {
+    window.onkeydown = function (e) {
+      return !(e.keyCode == 32 && e.target == document.body);
+    };
+
+    this.playButton.onclick = () => {
+      this.handlePlayButtonChange();
+    }
+
+    this.pauseButton.onclick = () => {
+      this.handlePlayButtonChange();
+    }
+
+    this.mb5sButton.onclick = () => {
+      this.moveBack5s();
+    }
+
+    this.fwd5sButton.onclick = () => {
+      this.forward5s();
+    }
+
+    /*this.spdButton.onclick = () => {
+      this.speed();
+    }*/
+
+    this.volumeOn.onclick = () => {
+      this.mute();
+      this.handleRangeUI(this.rangeInput, this.video.volume);
+    }
+
+    this.volumeOff.onclick = () => {
+      this.unMute();
+      this.handleRangeUI(this.rangeInput, this.video.volume);
+    }
+
+    this.fullScreenButton.onclick = () => {
+      this.fullScreen();
+    }
+
+    this.displayKnobsButton.onclick = () => {
+      this.displayKnobsButton.hidden = true;
+      this.hideKnobsButton.hidden = false;
+      this.hideKnobsButton.style.height = "auto";
+      this.hideKnobsButton.style.borderRadius = "initial";
+      this.subMVC1.style.height = "80%";
+      this.subMVC2.style.height = "19%";
+      this.knobsDiv.style.visibility = "visible";
+    }
+
+    this.hideKnobsButton.onclick = () => {
+      this.displayKnobsButton.hidden = false;
+      this.hideKnobsButton.hidden = true;
+      this.subMVC1.style.height = "96%";
+      this.subMVC2.style.height = "3%";
+      this.knobsDiv.style.visibility = "hidden"
+    }
+
+    this.displaySoundGraph.onclick = () => {
+      this.displaySoundGraph.hidden = true;
+      this.hideSoundGraph.hidden = false;
+      this.hideSoundGraph.style.height = "auto";
+      this.hideSoundGraph.style.borderRadius = "initial";
+      this.subVLC1.style.height = "80%";
+      this.subVLC2.style.height = "19%";
+      this.canvasDiv.hidden = false;
+    }
+
+    this.hideSoundGraph.onclick = () => {
+      this.displaySoundGraph.hidden = false;
+      this.hideSoundGraph.hidden = true;
+      this.canvasDiv.hidden = true;
+      this.subVLC1.style.height = "96%";
+      this.subVLC2.style.height = "3%";
+    }
+
+    this.listVideo.forEach(video => {
+      video.onclick = () => {
+        this.listVideo.forEach(vid => vid.classList.remove('active'));
+        video.classList.add('active');
+        if (video.classList.contains('active')) {
+          let src = video.children[0].getAttribute('src');
+          this.video.src = src;
+        }
+      }
+    });
+
+    this.progress.onclick = (e) => {
+      this.handleInputProgress(e);
+    }
+
+    this.video.onloadedmetadata = () => {
+      this.initializeVideo();
+      this.resetKnobs();
+    }
+
+    /* Event to manage the display of the video duration in the video list */
+    this.listVideo.forEach(video => {
+      const durationC = video.children[1].children[2];
+      video.children[0].onloadedmetadata = () => {
+        let videoDuration = Math.round(video.children[0].duration);
+        let time = this.formatTime(videoDuration);
+        let textnode = document.createTextNode(`${time.minutes}:${time.seconds}`);
+        durationC.replaceChild(textnode, durationC.children[0]);
+      }
+    });
+
+    /* Event to manage the update of time video and progress bar */
+    this.video.ontimeupdate = () => {
+      this.updateTimeElapsed();
+      this.updateProgressBar();
+    }
+
+    this.rangeInput.oninput = (e) => {
+      this.handleInputChange(e);
+    }
+
+    this.knob1.oninput = () => {
+      this.changeGain(this.knob1.value, 0);
+    }
+
+    this.knob2.oninput = () => {
+      this.changeGain(this.knob2.value, 1);
+    }
+
+    this.knob3.oninput = () => {
+      this.changeGain(this.knob3.value, 2);
+    }
+
+    this.knob4.oninput = () => {
+      this.changeGain(this.knob4.value, 3);
+    }
+
+    this.knob5.oninput = () => {
+      this.changeGain(this.knob5.value, 4);
+    }
+
+    this.knob6.oninput = () => {
+      this.changeGain(this.knob6.value, 5);
+    }
+
+    document.onkeyup = (e) => {
+      this.keyboardShortcuts(e);
+    }
+  }
+
+  forward5s() {
+    this.video.currentTime += 5;
+  }
+
+  moveBack5s() {
+    this.video.currentTime -= 5;
+  }
+
+  /*speed() {
+    this.video.playbackRate += 0.5;
+  }*/
+
+  mute() {
+    this.currentVolume = this.video.volume;
+    this.video.volume = 0;
+    this.rangeInput.value = 0;
+  }
+
+  unMute() {
+    console.log(this.currentVolume);
+    this.video.volume = this.currentVolume;
+    console.log(this.video.volume);
+    this.rangeInput.value = this.currentVolume;
+  }
+
+  resetVideo() {
+    this.video.currentTime = 0;
+  }
+
+  fullScreen() {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else if (document.webkitFullscreenElement) {
+      document.webkitExitFullscreen();
+    } else if (this.videoContainer.webkitRequestFullscreen) {
+      this.videoContainer.webkitRequestFullscreen();
+    } else {
+      this.videoContainer.requestFullscreen();
+    }
+  }
+
+  showControls() {
+    this.videoControls.style.transform = "translateY(0)";
+  }
+
+  hideControls() {
+    this.videoControls.style.removeProperty('transform');
+  }
+
+  formatTime(timeInSeconds) {
+    let result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
+    return {
+      minutes: result.substr(3, 2),
+      seconds: result.substr(6, 2),
+    };
+  }
+
+  initializeVideo() {
+    console.log(this.video.duration);
+    this.pauseButton.hidden = true;
+    this.playButton.hidden = false;
+    this.progressBar.style.width = "0";
+    this.showControls();
+    let videoDuration = Math.round(this.video.duration);
+    let time = this.formatTime(videoDuration);
+    this.duration.innerText = `${time.minutes}:${time.seconds}`;
+    //this.handlePlayButtonChange();
+  }
+
+  updateTimeElapsed() {
+    const time = this.formatTime(Math.round(this.video.currentTime));
+    this.timeElapsed.innerText = `${time.minutes}:${time.seconds}`;
+  }
+
+  updateProgressBar() {
+    const percentage = (this.video.currentTime / this.video.duration) * 100;
+    this.progressBar.style.width = `${percentage}%`;
+  }
+
+  handlePlayButtonChange() {
+    if (this.video.paused || this.video.ended) {
+      this.video.play();
+      setTimeout(() => {
+        this.hideControls();
+      }, 3000);
+      this.pauseButton.hidden = false;
+      this.playButton.hidden = true;
+    } else {
+      this.video.pause();
+      this.showControls();
+      this.pauseButton.hidden = true;
+      this.playButton.hidden = false;
+    }
+  }
+
+  handleRangeUI(element, value) {
+    if (value < 0.1) {
+      this.volumeOn.hidden = true;
+      this.volumeOff.hidden = false;
+    } else {
+      this.volumeOn.hidden = false;
+      this.volumeOff.hidden = true;
+    }
+    element.style.backgroundSize = (value - element.min) * 100 / (element.max - element.min) + '% 100%';
+  }
+
+  handleInputChange(e) {
+    let target = e.target;
+    const min = target.min
+    const max = target.max
+    const val = target.value
+    this.video.volume = val;
+    this.handleRangeUI(target, val);
+    console.log(val);
+  }
+
+  handleInputProgress(e) {
+    const progressTime = (e.offsetX / this.progress.offsetWidth) * this.video.duration;
+    this.video.currentTime = progressTime;
+  }
+
+  resetKnobs() {
+    let allKnobs = this.shadowRoot.querySelectorAll('.knobs-controls');
+    allKnobs.forEach(knobs => {
+      let knobChilds = knobs.childNodes;
+      knobChilds.forEach(child => {
+        if (child.nodeName.toLowerCase() == 'webaudio-knob') {
+          child.value = 0;
+        }
+        if (child.nodeName.toLowerCase() == 'output') {
+          child.value = 0;
+        }
+      });
+    });
+  }
+
+  keyboardShortcuts(event) {
+    const { key } = event;
+    switch (key) {
+      case ' ':
+        this.handlePlayButtonChange();
+        break;
+      case '0':
+        this.resetVideo();
+        break;
+      case 'ArrowLeft':
+        this.moveBack5s();
+        break;
+      case 'ArrowRight':
+        this.forward5s();
+        break;
+      case 'ArrowUp':
+        //this.moveBack5s();
+        break;
+      case 'ArrowDown':
+        //this.forward5s();
+        break;
+      case 'f':
+        this.fullScreen();
+        break;
+      case 'm':
+        if (this.video.volume > 0) {
+          this.mute();
+          this.handleRangeUI(this.rangeInput, this.video.volume)
+        } else {
+          this.unMute();
+          this.handleRangeUI(this.rangeInput, this.video.volume)
+        }
+        this.mute();
+        break;
+      case 'w':
+        //this.slowDown();
+        break;
+      case 'x':
+        //this.speedUp();
+        break;
+    }
+  }
+}
+
+customElements.define('my-component', MyVideoPlayer);
+
